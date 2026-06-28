d12741284da5fa310b7fbd395a73ab817eedab33
Nuhad Bin Aziz <nuhadbinaziz19@gmail.com>
2026-06-28 15:36:04 +0600

    Add files via upload

---
+++ 2236A - Games on the Train.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int mn = 1e9, mx = 0;
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            mn = min(mn, x);
+            mx = max(mx, x);
+        }
+        cout << mx - mn + 1 << "\n";
+    }
+    return 0;
+}
